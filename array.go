6c9f082ec351a3857c45a614b28887ae3196693d
ninglg <ninglg@gmail.com>
2015-08-25 13:29:32 +0800

    add array

---
+++ array.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+var arr [10]int	
+brr := [5]int {6, 7, 8, 9, 10}
+crr := [...]int {3:2, 4:1}
+
+for i:=0; i< 10; i++ {
+	arr[i] = i*2
+}
+fmt.Println(arr)
+
+fmt.Println(brr)
+
+fmt.Println(crr)
+
+}
