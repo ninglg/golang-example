a06e6d8e81f52c073762731b6af34917f03402f1
ninglg <ninglg@gmail.com>
2015-08-26 20:08:30 +0800

    add slice

---
+++ slice.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := [...]int{3: 4, 6: 9}
+
+	fmt.Println(i)
+
+	var j []int
+	j = i[:]
+	fmt.Println(j)
+
+	j = i[3:]
+	fmt.Println(j)
+
+	j = i[:6]
+	fmt.Println(j)
+
+	j = i[3:6]
+	fmt.Println(j)
+
+	j[2] = 7
+	fmt.Println(j)
+	fmt.Println(i)
+
+	fmt.Println(len(j))
+	fmt.Println(cap(j))
+
+	j = append(j, 8)
+	fmt.Println(j)
+	fmt.Println(i)
+
+	j = append(j, 6)
+	fmt.Println(j)
+	fmt.Println(i)
+}
