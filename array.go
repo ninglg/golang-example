fafe5214b9d68fa16ffb19b31d65bf81f24cf3cc
ninglg <ninglg@gmail.com>
2015-08-26 19:46:09 +0800

    modify array

--- array.go
+++ array.go
@@ -3,17 +3,19 @@ package main
 import "fmt"
 
 func main() {
-var arr [10]int	
-brr := [5]int {6, 7, 8, 9, 10}
-crr := [...]int {3:2, 4:1}
+	var arr [10]int
+	brr := [5]int{6, 7, 8, 9, 10}
+	crr := [...]int{3: 2, 4: 1}
 
-for i:=0; i< 10; i++ {
-	arr[i] = i*2
-}
-fmt.Println(arr)
+	for i := 0; i < 10; i++ {
+		arr[i] = i * 2
+	}
+	fmt.Println(arr)
 
-fmt.Println(brr)
+	fmt.Println(brr)
 
-fmt.Println(crr)
+	fmt.Println(crr)
+
+	fmt.Println(len(crr))
 
 }
