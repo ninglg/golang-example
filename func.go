3b0998dc7d9adef70d208ff8edb1afe70ca4151b
ninglg <ninglg@gmail.com>
2015-09-07 16:49:11 +0800

    modify func

--- func.go
+++ func.go
@@ -6,6 +6,16 @@ func main() {
 	test()
 }
 
+func sum(x int, y int) (s int) {
+	return x + y
+}
+
+func p(x int, y int) (int, int) {
+	return x, y
+}
+
 func test() {
 	fmt.Println("Hello, function!")
+	fmt.Println(sum(3, 4))
+	fmt.Println(p(6, 7))
 }
