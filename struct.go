60b715e160015ef638a6209fa83f6b02b7482408
ninglg <ninglg@gmail.com>
2015-09-07 13:27:48 +0800

    modify struct

--- struct.go
+++ struct.go
@@ -4,16 +4,29 @@ import (
 	"fmt"
 )
 
-type Person struct {
+type person struct {
 	name string
 	age  int
 }
 
 func main() {
-	x := new(Person)
+	x := new(person)
 	x.name = "XiaoMing"
 	x.age = 17
 
 	fmt.Println(x)
+	fmt.Println(*x)
 	fmt.Println(x.name)
+
+	var y person
+	y.name = "XiaoGang"
+	y.age = 23
+	fmt.Println(y)
+	fmt.Println(y.name)
+
+	z := person{"XiaoHong", 19}
+	fmt.Println(z)
+
+	g := person{age: 17, name: "XiaoZhang"}
+	fmt.Println(g)
 }
