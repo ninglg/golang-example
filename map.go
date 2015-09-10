b8b4120842c441c9a94b4d40426f7e48a48c988c
ninglg <ninglg@gmail.com>
2015-09-10 09:13:02 +0800

    modify map

--- map.go
+++ map.go
@@ -8,5 +8,18 @@ func main() {
 	i := make(map[string]int)
 	i["one"] = 1
 	i["two"] = 2
+	i["three"] = 3
 	fmt.Println(i)
+	fmt.Println(len(i))
+
+	fmt.Println(i["one"])
+
+	//
+	delete(i, "three")
+	fmt.Println(i)
+
+	//
+	a, b := i["two"]
+	fmt.Println(a, b)
+
 }
