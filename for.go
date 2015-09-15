0cca26c30d57a1b553e17eac3ceb2499a4f545c4
ninglg <ninglg@gmail.com>
2015-09-15 16:05:11 +0800

    modify go

--- for.go
+++ for.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i< 10; i++ {
+	for i := 0; i < 10; i++ {
 		fmt.Println(i)
+	}
+
+	for j, k := 0, 0; j+k < 100; j, k = j+5, k+10 {
+		fmt.Println(j, k)
 	}
 }
