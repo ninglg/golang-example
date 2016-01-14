75b85e4fc746217b8bf8323cb569bfaa456df747
Light Ning <ninglg@gmail.com>
2016-01-14 19:51:17 +0800

    add helloserver

---
+++ helloserver.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+}
+
+func main() {
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
