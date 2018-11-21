fd16c1e3347ed49950bf792eaf470f3d6553dbf8
ninglg <ninglg@gmail.com>
2018-11-22 01:10:21 +0800

    update

--- json.go
+++ json.go
@@ -5,19 +5,19 @@ import (
 	"fmt"
 )
 
-type Server struct {
-	ServerName string
-	ServerIP   string
+type Srv struct {
+	SrvName string
+	SrvIP   string
 }
 
-type Serverslice struct {
-	Servers []Server
+type SrvSlice struct {
+	SrvS []Srv
 }
 
 func main() {
-	var s Serverslice
-	s.Servers = append(s.Servers, Server{ServerName: "Shanghai_VPN", ServerIP: "127.0.0.1"})
-	s.Servers = append(s.Servers, Server{ServerName: "Beijing_VPN", ServerIP: "127.0.0.2"})
+	var s SrvSlice
+	s.SrvS = append(s.SrvS, Srv{SrvName: "Shanghai_VPN", SrvIP: "127.0.0.1"})
+	s.SrvS = append(s.SrvS, Srv{SrvName: "Beijing_VPN", SrvIP: "127.0.0.2"})
 	b, err := json.Marshal(s)
 	if err != nil {
 		fmt.Println("json err:", err)
