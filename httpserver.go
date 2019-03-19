0df0559083982e6a39c4606c9c410d34763b1d79
ninglg <ninglg@gmail.com>
2019-03-19 16:10:10 +0800

    update

--- httpserver.go
+++ httpserver.go
@@ -5,11 +5,22 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "hello, world!\n")
 	r.ParseForm()
+
+	fmt.Println(r.Form)
+	fmt.Println("path : ", r.URL.Path)
+	fmt.Println("scheme : ", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+
 	fmt.Fprintln(w, r.Form)
 }
 
