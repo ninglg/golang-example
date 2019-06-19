58433a434d5d3547758b56ea20855a29751bc4c2
ninglg <ninglg@gmail.com>
2019-06-19 14:16:23 +0800

    update

--- http.go
+++ http.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
 )
 
 func httpGet() {
@@ -17,23 +19,74 @@ func httpGet() {
 		fmt.Println(resp.StatusCode)
 	}
 
-	buf := make([]byte, 1024)
-	f, err := os.OpenFile("baidu.html", os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	defer f.Close()
 
-	for {
-		n, _ := resp.Body.Read(buf)
-		if 0 == n {
-			break
-		}
-
-		f.WriteString(string(buf[:n]))
-	}
+	fmt.Println(string(body))
 }
+
+func httpPost() {
+	resp, err := http.Post("http://www.baidu.com",
+		"application/x-www-form-urlencoded",
+		strings.NewReader("name=test"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(body))
+}
+
+func httpPostForm() {
+	resp, err := http.PostForm("http://www.baidu.com",
+		url.Values{"key": {"Value"}, "id": {"123"}})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(body))
+}
+
+func httpDo() {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", "http://www.baidu.com", strings.NewReader("name=test"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Cookie", "name=test")
+
+	resp, err := client.Do(req)
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(body))
+}
+
 func httpFunc() {
 	fmt.Println("========== httpFunc ==========")
-	httpGet()
+	go httpGet()
+	go httpPost()
+	go httpPostForm()
+	go httpDo()
 }
