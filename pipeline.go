cdc316d0d77c8d5b441bced15213d8481d75b0ee
ninglg <ninglg@gmail.com>
2019-07-09 20:16:51 +0800

    update

--- pipeline.go
+++ pipeline.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func producer(nums ...int) <-chan int {
@@ -29,15 +30,52 @@ func square(inCh <-chan int) <-chan int {
 	return out
 }
 
+func merge(cs ...<-chan int) <-chan int {
+	out := make(chan int)
+
+	var wg sync.WaitGroup
+
+	collect := func(in <-chan int) {
+		defer wg.Done()
+		for n := range in {
+			out <- n
+		}
+	}
+
+	wg.Add(len(cs))
+
+	// FAN-IN
+	for _, c := range cs {
+		go collect(c)
+	}
+
+	// 错误方式：直接等待是bug，死锁，因为merge写了out，main却没有读
+	// wg.Wait()
+	// close(out)
+
+	// 正确方式
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
+
 func pipelineFunc() {
 	fmt.Println("========== pipelineFunc ==========")
 
-	in := producer(1, 2, 3, 4, 5)
-	ch := square(in)
+	in := producer(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+	// FAN-OUT
+	c1 := square(in)
+	c2 := square(in)
+	c3 := square(in)
 
 	// consumer
-	for ret := range ch {
+	for ret := range merge(c1, c2, c3) {
 		fmt.Printf("%d\t", ret)
 	}
+
 	fmt.Println()
 }
