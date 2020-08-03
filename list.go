0486b1c374e5c77ed4f0b0616baebcd3da90da99
ninglg <ninglg@gmail.com>
2020-08-03 20:01:24 +0800

    update

---
+++ list.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("========== ListFunc ==========")
+
+	tmpList := list.New()
+
+	for i := 1; i <= 10; i += 2 {
+		tmpList.PushBack(i)
+	}
+
+	tmpList.PushFront(0)
+
+	for j := tmpList.Front(); j != nil; j = j.Next() {
+		fmt.Println(j.Value)
+	}
+}
+
