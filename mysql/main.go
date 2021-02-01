eca3d4535ef514f7311be1781cd9fb1c7fc8d67d
ninglg <ninglg@gmail.com>
2021-02-01 23:22:06 +0800

    update

--- mysql/main.go
+++ mysql/main.go
@@ -3,48 +3,38 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 
-//type student struct {
-//	stuId int ``
-//	stuName string 	``
-//}
-
 func init() {
-	var err error
-
 	// DSN:Data Source Name
 	dsn := "root:123456@tcp(127.0.0.1:3306)/school?charset=utf8mb4&parseTime=True"
 	// sql.Open仅创建DB对象，不会打开与数据库的任何连接，即不会校验账号密码是否正确。
 	// 注意！这里不要使用:=，此处是给全局变量赋值，然后在main函数中使用全局变量db。
-	db, err = sql.Open("mysql", dsn)
-	if err != nil {
-		log.Panicln("error:", err.Error())
-	}
+	db, _ = sql.Open("mysql", dsn)
 
-	db.SetMaxIdleConns(10)
+	// 设置数据库最大连接数
 	db.SetMaxOpenConns(10)
+	// 设置数据库最大空闲连接数
+	db.SetMaxIdleConns(10)
 }
 
-func main() {
-	var err error
-
-	defer db.Close()
+func ping() {
 	// 如果要测试连接，必须执行查询以强制打开连接。常见的方法是在数据库对象上调用Ping()。
-	err = db.Ping()
+	err := db.Ping()
 	if err != nil {
 		fmt.Println("db.Ping error:", err.Error())
 		return
 	}
 
 	fmt.Println("db.Ping success!")
+}
 
-	// 0.使用db.Exec方法来进行Delete
+func deleteSql() {
+	// 使用db.Exec方法来进行Delete
 	result, err := db.Exec("delete from student")
 	if err != nil {
 		fmt.Println("db.Exec delete error:", err.Error())
@@ -53,9 +43,11 @@ func main() {
 
 	rowsAffected, _ := result.RowsAffected()
 	fmt.Println("db.Exec delete rowsAffected is:", rowsAffected)
+}
 
-	// 1.使用db.Exec方法来直接进行Insert
-	result, err = db.Exec("insert student (stu_id, stu_name) values (?, ?)", 100, "xiaogang")
+func insertSql() {
+	// 使用db.Exec方法来直接进行Insert
+	result, err := db.Exec("insert student (stu_id, stu_name) values (?, ?)", 100, "xiaogang")
 	if err != nil {
 		fmt.Println("db.Exec insert error:", err.Error())
 		return
@@ -63,7 +55,9 @@ func main() {
 
 	lastId, _ := result.LastInsertId()
 	fmt.Println("db.Exec insert lastId is: ", lastId)
+}
 
+func insertSql2() {
 	// 2.使用Prepare方法和stmt的Exec方法来进行Insert
 	stmt, err := db.Prepare("insert student (stu_id, stu_name) values (?, ?)")
 	if err != nil {
@@ -73,27 +67,34 @@ func main() {
 	// 资源关闭
 	defer stmt.Close()
 
-	result, _ = stmt.Exec(200, "xiaoming")
-	lastId, _ = result.LastInsertId()
+	result, _ := stmt.Exec(200, "xiaoming")
+	lastId, _ := result.LastInsertId()
 	fmt.Println("db.Prepare stmt.Exec insert lastId is: ", lastId)
+}
 
-	// 3.使用db.Exec方法来进行Update
-	result, err = db.Exec("update student set stu_name='xiaoqiang' where stu_id = ?", 200)
+func updateSql() {
+	// 使用db.Exec方法来进行Update
+	result, err := db.Exec("update student set stu_name='xiaoqiang' where stu_id = ?", 200)
 	if err != nil {
 		fmt.Println("db.Exec update error:", err.Error())
 		return
 	}
 
-	rowsAffected, _ = result.RowsAffected()
+	rowsAffected, _ := result.RowsAffected()
 	fmt.Println("db.Exec update rowsAffected is:", rowsAffected)
 
-	// 4.使用db.QueryRow查询一条记录
+}
+
+func queryRow() {
+	// 使用db.QueryRow查询一条记录
 	var stuId int
 	var stuName string
 	db.QueryRow("select stu_id, stu_name from student where stu_id = ?", 100).Scan(&stuId, &stuName)
 	fmt.Println("db.QueryRow:", stuId, stuName)
+}
 
-	// 5.使用db.Query查询多条记录
+func query() {
+	// 使用db.Query查询多条记录
 	rows, err := db.Query("select stu_id, stu_name from student")
 	if err != nil {
 		fmt.Println("db.Query rows error:", err.Error())
@@ -113,4 +114,17 @@ func main() {
 		fmt.Println("db.Query rows.Next:", id, name)
 	}
 
+	rows.Close()
+}
+
+func main() {
+	defer db.Close()
+
+	ping()
+	deleteSql()
+	insertSql()
+	insertSql2()
+	updateSql()
+	queryRow()
+	query()
 }
