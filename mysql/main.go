e8d02c35b42138118dae09e9d7f3ed6ae0754814
ninglg <ninglg@gmail.com>
2021-02-01 18:19:32 +0800

    update

--- mysql/main.go
+++ mysql/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
+
+//type student struct {
+//	stuId int ``
+//	stuName string 	``
+//}
 
 func init() {
 	var err error
@@ -18,7 +24,7 @@ func init() {
 	// 注意！这里不要使用:=，此处是给全局变量赋值，然后在main函数中使用全局变量db。
 	db, err = sql.Open("mysql", dsn)
 	if err != nil {
-		log.Panicln("err:", err.Error())
+		log.Panicln("error:", err.Error())
 	}
 
 	db.SetMaxIdleConns(10)
@@ -26,18 +32,84 @@ func init() {
 }
 
 func main() {
+	var err error
+
 	// 如果要测试连接，必须执行查询以强制打开连接。常见的方法是在数据库对象上调用Ping()。
-	err := db.Ping()
+	err = db.Ping()
 	if err != nil {
-		log.Panic("err:", err.Error())
+		fmt.Println("db.Ping error:", err.Error())
+		return
 	}
 
-	// Delete
+	fmt.Println("db.Ping success!")
 
-	// Insert
+	// 0.使用db.Exec方法来进行Delete
+	result, err := db.Exec("delete from student")
+	if err != nil {
+		fmt.Println("db.Exec delete error:", err.Error())
+		return
+	}
 
-	// Query
+	rowsAffected, _ := result.RowsAffected()
+	fmt.Println("db.Exec delete rowsAffected is:", rowsAffected)
 
-	// Update
+	// 1.使用db.Exec方法来直接进行Insert
+	result, err = db.Exec("insert student (stu_id, stu_name) values (?, ?)", 100, "xiaogang")
+	if err != nil {
+		fmt.Println("db.Exec insert error:", err.Error())
+		return
+	}
 
+	lastId, _ := result.LastInsertId()
+	fmt.Println("db.Exec insert lastId is: ", lastId)
+
+	// 2.使用Prepare方法和stmt的Exec方法来进行Insert
+	stmt, err := db.Prepare("insert student (stu_id, stu_name) values (?, ?)")
+	if err != nil {
+		fmt.Println("db.Prepare error:", err.Error())
+		return
+	}
+	// 资源关闭
+	defer stmt.Close()
+
+	result, _ = stmt.Exec(200, "xiaoming")
+	lastId, _ = result.LastInsertId()
+	fmt.Println("db.Prepare stmt.Exec insert lastId is: ", lastId)
+
+	// 3.Update
+	result, err = db.Exec("update student set stu_name='xiaoqiang' where stu_id = ?", 200)
+	if err != nil {
+		fmt.Println("db.Exec update error:", err.Error())
+		return
+	}
+
+	rowsAffected, _ = result.RowsAffected()
+	fmt.Println("db.Exec update rowsAffected is:", rowsAffected)
+
+	// 4.Query查询一条记录
+	var stuId int
+	var stuName string
+	db.QueryRow("select stu_id, stu_name from student where stu_id = ?", 100).Scan(&stuId, &stuName)
+	fmt.Println("db.QueryRow:", stuId, stuName)
+
+	// 5.Query查询多条记录
+	rows, err := db.Query("select stu_id, stu_name from student")
+	if err != nil {
+		fmt.Println("db.Query rows error:", err.Error())
+		return
+	}
+
+	for rows.Next() {
+		var (
+			id int
+			name string
+		)
+		if err = rows.Scan(&id, &name); err != nil {
+			fmt.Println("db.Query rows.Next error:", err.Error())
+			continue
+		}
+
+		fmt.Println("db.Query rows.Next:", id, name)
+	}
+	
 }
--- mysql/student.sql
+++ mysql/student.sql
@@ -1,6 +1,6 @@
 use school;
 
-#drop table student;
+drop table student;
 
 CREATE TABLE `student` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
@@ -8,6 +8,7 @@ CREATE TABLE `student` (
   `stu_name` varchar(20) NOT NULL DEFAULT "" COMMENT '学生姓名',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_stuid` (`stu_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='学生表';
 
