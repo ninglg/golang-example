c5dbe829abc518f4aeea4671402514bc9c1c8c2e
ninglg <ninglg@gmail.com>
2021-02-02 17:09:52 +0800

    update

--- redis/main.go
+++ redis/main.go
@@ -1,5 +1,50 @@
 package main
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var ctx = context.Background()
+var rdb *redis.Client
+
+func init() {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+}
+
+func setString() {
+	err := rdb.Set(ctx, "hello", "world", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getString() {
+	val, err := rdb.Get(ctx, "hello").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("hello:", val)
+}
+
+func getNotExistString() {
+	val2, err := rdb.Get(ctx, "hello2").Result()
+	if err == redis.Nil {
+		fmt.Println("hello2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("hello2", val2)
+	}
+}
 
 func main() {
-	
+	setString()
+	getString()
+	getNotExistString()
 }
