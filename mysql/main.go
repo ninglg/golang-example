ad7c372d9880bbd88743cbb2a40b073c3a5f29ee
ninglg <ninglg@gmail.com>
2021-02-03 17:40:27 +0800

    update

--- mysql/main.go
+++ mysql/main.go
@@ -13,11 +13,13 @@ import (
 var db *sql.DB
 
 func init() {
-	// DSN:Data Source Name
-	dsn := "root:123456@tcp(127.0.0.1:3306)/school?charset=utf8mb4&parseTime=True"
+	// Driver Name
+	driverName := "mysql"
+	// DSN: Data Source Name
+	dataSourceName := "root:123456@tcp(127.0.0.1:3306)/school?charset=utf8mb4&parseTime=True"
 	// sql.Open仅创建DB对象，不会打开与数据库的任何连接，即不会校验账号密码是否正确。
 	// 注意！这里不要使用:=，此处是给全局变量赋值，然后在main函数中使用全局变量db。
-	db, _ = sql.Open("mysql", dsn)
+	db, _ = sql.Open(driverName, dataSourceName)
 
 	// 设置数据库最大连接数
 	db.SetMaxOpenConns(10)
