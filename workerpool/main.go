077447a527f72666d791a7bb2e101cf09208d0c8
ninglg <ninglg@gmail.com>
2021-02-04 16:05:06 +0800

    update

--- workerpool/main.go
+++ workerpool/main.go
@@ -1,6 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func process(i int) {
+	fmt.Printf("%d ", i)
+}
+
+func workerpool() {
+	fmt.Println("============ workerpool ============")
+	startTime := time.Now()
+	channel := make(chan int, 10)
+	var wg sync.WaitGroup
+	// 5个worker
+	var workerPoolSize = 5
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < workerPoolSize; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for a := range channel {
+					process(a)
+					time.Sleep(time.Second)
+				}
+			}()
+		}
+	}()
+
+	// 30个task
+	for i := 0; i < 30; i++ {
+		channel <- i
+	}
+	close(channel)
+	wg.Wait()
+
+	elapsed := time.Since(startTime)
+
+	fmt.Printf("\nworkerpool1 took %s to finish\n", elapsed)
+}
+
 func main() {
-	workerpool1()
-	//workerpool2()
+	workerpool()
 }
