e81efcecb49a708e83fd88a0368700312e9be29d
ninglg <ninglg@gmail.com>
2022-02-23 15:36:45 +0800

    update

--- basic-syntax/strconv.go
+++ basic-syntax/strconv.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
+	s := "123"
+	a, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(a)
+
+	k := strconv.FormatInt(int64(a), 10)
+	fmt.Println(k)
 
 }
