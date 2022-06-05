f1da5157cf6e1116f95a57c08f3303e8120c8afd
ninglg <ninglg@gmail.com>
2022-06-05 18:55:53 +0800

    update

--- basic-syntax/httpserver.go
+++ basic-syntax/httpserver.go
@@ -12,13 +12,13 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	_, _ = io.WriteString(w, "hello, world!\n")
 	_ = r.ParseForm()
 
-	fmt.Println(r.Form)
-	fmt.Println("path : ", r.URL.Path)
-	fmt.Println("scheme : ", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
+	fmt.Fprintln(w, r.Form)
+	fmt.Fprintln(w, "r.URL.Path: "+r.URL.Path)
+	fmt.Fprintln(w, "r.URL.Scheme: "+r.URL.Scheme)
+	fmt.Fprintln(w, r.Form["url_long"])
 	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
+		fmt.Fprintln(w, "key:", k)
+		fmt.Fprintln(w, "val:", strings.Join(v, ""))
 	}
 
 	_, _ = fmt.Fprintln(w, r.Form)
@@ -26,8 +26,9 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/hi", hello)
-	err := http.ListenAndServe(":9090", nil)
+	err := http.ListenAndServe(":9091", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
+
