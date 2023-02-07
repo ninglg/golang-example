7e0b6c74c6533b2320d6e0e90a185ad98a1ecbf2
ningliang <ningliang@sixents.com>
2023-02-07 15:14:13 +0800

    update

--- basic-syntax/channel.go
+++ basic-syntax/channel.go
@@ -40,8 +40,13 @@ func main() {
 	向以关闭channel发送消息会panic
 	从已关闭channel读取数据，不会panic，若存在数据，则可以读出未被读取的消息，若已被读出，则获取的数据为零值，可以通过ok-idiom的方式，判断channel是否关闭
 	channel的关闭操作，会产生广播消息，所有向channel读取消息的goroutine都会接受到消息
+	================================================================
+	有些 channel 并不需要主动去关闭，因为 GC 会判断 channel 的状态并对闲置 channel 进行回收。
+	只有那些需要明确告知相关方 channel 已关闭的场景，才需要主动进行 channel 的关闭，比如 for range 。
 	 */
 	close(ch)
+
+
 	//for {
 	//	if v, ok := <-ch; ok {
 	//		fmt.Printf("%d ", v)
@@ -65,4 +70,19 @@ func main() {
 	有缓存的channel使用环形数组实现，当缓存未满时，向channel发送消息不会阻塞，当缓存满时，发送操作会阻塞，
 	直到其他goroutine从channel中读取消息；同理，当channel中消息不为空时，读取消息不会阻塞，当channel为空时，读取操作会阻塞，直至其他goroutine向channel发送消息。
 	*/
+
+	// 在某些应用场景，工作goroutine一直处理事务，直到收到退出信号
+	mch := make(chan struct{})
+	quit := make(chan struct{})
+	for {
+		select {
+		case <- mch:
+			// 正常工作
+			//work()
+		case <- quit:
+			// 退出前，处理收尾工作
+			//doFinish()
+			return
+		}
+	}
 }
