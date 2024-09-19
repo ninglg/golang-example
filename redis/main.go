6a978b9cafa2fa049f7dd00714a7928a71444500
ningliang <ningliang@sixents.com>
2024-09-19 14:05:30 +0800

    update

--- redis/main.go
+++ redis/main.go
@@ -1,13 +1,17 @@
 package main
+
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/go-redis/redis/v8"
 )
 
-var ctx = context.Background()
-var rdb *redis.Client
+var (
+	ctx = context.Background()
+	rdb *redis.Client
+)
 
 func init() {
 	rdb = redis.NewClient(&redis.Options{
@@ -16,41 +20,44 @@ func init() {
 		DB:       0,  // use default DB
 	})
 
-	result, err := rdb.Ping(ctx).Result()
-	if err != nil {
-		panic(err.Error())
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		log.Fatalf("Failed to connect to Redis: %v", err)
 	}
-	fmt.Println("ping:", result)
+	fmt.Println("Connected to Redis")
 }
 
-func setString() {
-	err := rdb.Set(ctx, "hello", "world", 0).Err()
-	if err != nil {
-		panic(err)
-	}
+func setString(key, value string) error {
+	return rdb.Set(ctx, key, value, 0).Err()
 }
 
-func getString() {
-	val, err := rdb.Get(ctx, "hello").Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("hello:", val)
-}
-
-func getNotExistString() {
-	val2, err := rdb.Get(ctx, "hello2").Result()
-	if err == redis.Nil {
-		fmt.Println("hello2 does not exist")
-	} else if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("hello2", val2)
-	}
+func getString(key string) (string, error) {
+	return rdb.Get(ctx, key).Result()
 }
 
 func main() {
-	setString()
-	getString()
-	getNotExistString()
+	if err := setString("hello", "world"); err != nil {
+		log.Printf("Failed to set string: %v", err)
+	}
+
+	val, err := getString("hello")
+	if err != nil {
+		if err == redis.Nil {
+			fmt.Println("Key 'hello' does not exist")
+		} else {
+			log.Printf("Failed to get string: %v", err)
+		}
+	} else {
+		fmt.Printf("hello: %s\n", val)
+	}
+
+	val, err = getString("hello2")
+	if err != nil {
+		if err == redis.Nil {
+			fmt.Println("Key 'hello2' does not exist")
+		} else {
+			log.Printf("Failed to get string: %v", err)
+		}
+	} else {
+		fmt.Printf("hello2: %s\n", val)
+	}
 }
